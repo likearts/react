a2acf27594442ae3db5e65aa9fc329453a7039cd
haewon park <likeartser@gmail.com>
2019-09-22 00:55:08 +0900

    Updates

---
+++ precache-manifest.6ad403ed95fedd8e8dabfb0389c7e0e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7076ed344c00b1228ad13475fe8ff47",
+    "url": "/./index.html"
+  },
+  {
+    "revision": "3c688199f04d545f9e8b",
+    "url": "/./static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "382ad0721d792a7da91c",
+    "url": "/./static/js/2.eff423a0.chunk.js"
+  },
+  {
+    "revision": "3c688199f04d545f9e8b",
+    "url": "/./static/js/main.0e857ab8.chunk.js"
+  },
+  {
+    "revision": "4ce14cf35a6783ae2c28",
+    "url": "/./static/js/runtime-main.955fb69d.js"
+  }
+]);
