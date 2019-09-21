e03bad5354fe638b809f293a0f6dbe67cfe771bf
likearts <likearts@naver.com>
2019-09-22 00:45:28 +0900

    Update precache-manifest.9d152b8ebc14dce52e8b83cabb05dd32.js

--- precache-manifest.9d152b8ebc14dce52e8b83cabb05dd32.js
+++ precache-manifest.9d152b8ebc14dce52e8b83cabb05dd32.js
@@ -1,22 +1,22 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "2d05115a5290ed357d9bd277a7884c10",
-    "url": "/index.html"
+    "url": "./index.html"
   },
   {
     "revision": "c0da7cdfe86388b96f38",
-    "url": "/static/css/main.b100e6da.chunk.css"
+    "url": "./static/css/main.b100e6da.chunk.css"
   },
   {
     "revision": "382ad0721d792a7da91c",
-    "url": "/static/js/2.eff423a0.chunk.js"
+    "url": "./static/js/2.eff423a0.chunk.js"
   },
   {
     "revision": "c0da7cdfe86388b96f38",
-    "url": "/static/js/main.6316b894.chunk.js"
+    "url": "./static/js/main.6316b894.chunk.js"
   },
   {
     "revision": "22eb420095fcc744f0f4",
-    "url": "/static/js/runtime-main.c2124f62.js"
+    "url": "./static/js/runtime-main.c2124f62.js"
   }
 ]);
