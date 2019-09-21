141ff43548381f6d42c29b0cd0284c07bc6626ab
likearts <likearts@naver.com>
2019-09-22 00:45:48 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9d152b8ebc14dce52e8b83cabb05dd32.js"
+  "./precache-manifest.9d152b8ebc14dce52e8b83cabb05dd32.js"
 );
 
 self.addEventListener('message', (event) => {
